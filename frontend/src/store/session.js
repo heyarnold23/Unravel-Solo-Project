389085688d3700ddc75d8ef1a170b869804e443d
Arnold C <acabang23@gmail.com>
2021-09-10 13:44:40 -0700

    made signup action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -31,12 +31,25 @@ export const login = (user) => async (dispatch) => {
 };
 
 export const restoreUser = () => async (dispatch) => {
-  
   const response = await fetch('/api/session');
   const data = await response.json();
   dispatch(setUser(data.user));
   return response;
+};
 
+export const signup = (formData) => async (dispatch) => {
+  const {username, email, password} = formData;
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      email,
+      password
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
 };
 
 const initialState = { user: null };
