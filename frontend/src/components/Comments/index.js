b0f8fbfd66074d7b82a41d0617f4358c27616f4d
Arnold C <acabang23@gmail.com>
2021-09-14 15:39:32 -0700

    comments showing on songpage now

--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -6,21 +6,40 @@ import {getComments} from '../../store/comments';
 import styles from './Comments.module.css'
 
 export default function Comments(){
-    const dispatch = useDispatch();
     const {id} = useParams();
+    const dispatch = useDispatch();
+    const commentsObject = useSelector(state => state.comments)
+    const commentsArr = Object.values(commentsObject)
 
     useEffect(() => {
         dispatch(getComments(id))
     },[dispatch, id])
 
-    const commentsObject = useSelector(state => state.comments)
-    const comments = commentsObject[id]
-    console.log('commeeennttsssss',comments);
+    // const comments = commentsObject[id]
+    console.log('commeeennttsssss',commentsArr);
     //comments is an object id, userId, songId, body
+
+    //IMPLEMENT A .FIND TO FIND THE USER NAME
 
     return (
         <>
-            <div>{comments.body}</div>
+            <div>
+            FORM PLACEHOLDER
+            </div>
+            <div className={styles.commentBox}>
+                <div className={styles.commentList}>
+                    <ul className={styles.commentUl}>
+                        {commentsArr.map((comment) =>
+                        <li key={comment.id} className={styles.commentLi}>
+                            <p className={styles.userName}>UserName PLACEHOLDER</p>
+                            <p className={styles.body}>{comment.body}</p>
+                            {/* placeholder for edit and delete buttons */}
+                        </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+
         </>
     )
 
