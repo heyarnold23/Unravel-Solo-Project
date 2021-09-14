e41997abb1be1c70542913dd67f512424664e259
Arnold C <acabang23@gmail.com>
2021-09-14 10:26:58 -0700

    working on refresh bug

--- frontend/src/components/SongPage/SongPage.js
+++ frontend/src/components/SongPage/SongPage.js
@@ -10,25 +10,26 @@ export default function SongPage(){
     const songsObject = useSelector(state => state.songs);
     const song = songsObject[id]
 
+    console.log(id)
     console.log('songPage',song);
 
     return(
         <>
             <div className={styles.headContainer}
-                style={{ backgroundImage: `url(${song.backgroundArtUrl})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
+                style={{ backgroundImage: `url(${song?.backgroundArtUrl})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
             >
                 <div className={styles.inContainer}>
                     <div className={styles.songInfoContainer}>
                         <div id={styles.albumImgDiv}>
-                            <img src={`${song.albumUrl}`} alt="" />
+                            <img src={`${song?.albumUrl}`} alt="" />
                         </div>
                         <div id={styles.songDetails}>
-                            {song.name}
+                            {song?.name}
                             <div id={styles.artistInfo}>
-                                {song.artistName}
+                                {song?.artistName}
                             </div>
                             <div id={styles.songInfo}>
-                                <span style={{color: `#9A9A9A`}}>Album</span> {song.albumName}
+                                <span style={{color: `#9A9A9A`}}>Album</span> {song?.albumName}
                             </div>
                         </div>
                     </div>
@@ -36,7 +37,7 @@ export default function SongPage(){
             </div>
             <div id={styles.bodyDiv}>
                 <div id={styles.inBodyDiv}>
-                    {song.body}
+                    {song?.body}
                 </div>
 
 
