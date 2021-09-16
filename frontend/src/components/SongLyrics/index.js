e5482604f730adbf485707f63665983e38976381
Arnold C <acabang23@gmail.com>
2021-09-16 10:39:41 -0700

    highlighting the body will console.log  the indices

--- frontend/src/components/SongLyrics/index.js
+++ frontend/src/components/SongLyrics/index.js
@@ -6,13 +6,62 @@ import {getSongs} from '../../store/songs';
 
 export default function SongLyrics({body}){
 
+    const work = {body}
 
+    const arr = Object.values(work)
+
+    const string = arr.toString();
+
+    // console.log('this is string',string.length);
+
+    const split = string.split('')
+
+    console.log('this is split', split );
+    console.log('this is index', split[12]);
+
+
+
+    /** */
+
+    const highlight = () => {
+
+        // const sel = window.getSelection()
+        // const selStr = window.getSelection.toString()
+        // const range = sel.getRangeAt(0).getBoundingClientRect()
+        // console.log(sel);
+        // console.log(selStr);
+        // console.log(range);
+
+        const selection = window.getSelection();
+        const start = selection.anchorOffset;
+        const end = selection.focusOffset;
+        if (start >= 0 && end >= 0){
+    	    console.log("start: " + start);
+    	    console.log("end: " + end);
+        }
+
+    }
+
+    /** */
 
 
     return (
     <>
-    <p>{body}</p>
+    {/* {split.map((s) => <span>{s}</span> )} */}
 
+
+
+
+
+
+    {/* {split.map((s) => <span onMouseUp={highlight}>{s}</span> )} */}
+    {/* <span onMouseUp={highlight}>{split}</span> */}
+    <span onMouseUp={highlight}>{body}</span>
+
+        {/* maybe on each span, have an onclick or other event handler
+        that will equal to a function above that has window.getSelection()
+        underneath it
+        */}
     </>
     )
 }
