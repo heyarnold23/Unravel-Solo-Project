8f399a2c4518574cc93f5abd75fa235be4fb9ad6
Arnold C <acabang23@gmail.com>
2021-09-16 18:30:47 -0700

    making progress on read annotation

--- frontend/src/components/SongLyrics/index.js
+++ frontend/src/components/SongLyrics/index.js
@@ -3,6 +3,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getAnnotations } from '../../store/annotations';
 
+import styles from './SongLyrics.module.css'
 
 
 export default function SongLyrics({body}){
@@ -11,22 +12,26 @@ export default function SongLyrics({body}){
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getAnnotations(id))
+        dispatch(getAnnotations(id));
     },[dispatch, id])
 
     const annotationsObject = useSelector(state => state.annotations)
     const annoArr = Object.values(annotationsObject)
+    // [0: {id: 1, userId: 1, songId: 1, body: 'Well said
 
-    console.log('annoarr -------->>>>>>>',annoArr);
+    /*******/
+    // console.log('annoarr -------->>>>>>>',annoArr);
+    // console.log('this is how to get start and end pos --->>>>>', annoArr[0]?.startPos, annoArr[0]?.endPos);
+    /*******/
 
-    const work = {body}
-    const arr = Object.values(work)
-    const string = arr.toString();
-    // console.log('this is string',string.length);
-    const split = string.split('')
-    console.log('this is split', split );
-    console.log('this is index', split[12]);
 
+    // const work = {body}
+    // const arr = Object.values(work)
+    // const string = arr.toString();
+    // // console.log('this is string',string.length);
+    // const split = string.split('')
+    // console.log('this is split', split );
+    // console.log('this is index', split[20]);
 
 
     /** */
@@ -39,37 +44,86 @@ export default function SongLyrics({body}){
         // console.log(sel);
         // console.log(selStr);
         // console.log(range);
-
-        const selection = window.getSelection();
+        const selection = document.getSelection();
+        // const selRange = selection.getRangeAt()
         const start = selection.anchorOffset;
         const end = selection.focusOffset;
         if (start >= 0 && end >= 0){
     	    console.log("start: " + start);
     	    console.log("end: " + end);
+            // console.log('selRange: ' + selRange);
         }
+        //also maybe can do something with useState and controlledInput and onChange or mouseUp
+        //maybe can run highlighted function from below
 
     }
 
+    /****************/
+    /* !!!!! highlighted can index into body and do something !!!!!!! */
+    const highlighted = (start, end) => {
+        //maybe make this an async somehow?
+
+        // const experiment = document.getElementsByTagName('span')[start]
+
+        // if (start >= 0 && end >= 0){
+        for (let i = start; i <= end; i++) {
+            const element = i;
+            const change = body?.[element];
+            console.log(change);
+            // change.style.backgroundColor = "red";
+        }
+    }
+    //maybe for loop to do effect of adding grey start to end?
+    //maybe apply className to give it highlight and click
+    // console.log(highlighted(18, 47));
+    // console.log('this is the map we want',annoArr.forEach((anno) => ))
+    annoArr.forEach(anno => {
+        console.log('annoArr and highlighted at work --->>>>');
+        console.log(anno.startPos, anno.endPos);
+        highlighted(anno.startPos, anno.endPos)
+    });
+    /****************/
+
+
     /** */
+    // proof that we can index into body if we async it
+    // const indexing = async (i) => (
+    //     console.log('indexing ----->>', body[i])
+    // );
+    // console.log(indexing(2));
+    /** */
+
+
+    // const experiment = document.getElementsByTagName('span')[4]
+    // console.log('experiment ------> ',experiment);
+
+
+
 
 
     return (
     <>
-    {/* {split.map((s) => <span>{s}</span> )} */}
+        {/* {split.map((s) => <span onMouseUp={highlight}>{s}</span> )} */}
+
+        {/* {split.map((s) => <span onMouseUp={highlight}>{s}</span> )} */}
+        {/* <span onMouseUp={highlight}>{split}</span> */}
+
+        {/* best one, line below */}
+        <span onMouseUp={highlight}>{body}</span>
 
 
+        {/* <body>
+            <span>
+                {body}
+            </span>
+
+        </body> */}
 
 
-
-
-    {/* {split.map((s) => <span onMouseUp={highlight}>{s}</span> )} */}
-    {/* <span onMouseUp={highlight}>{split}</span> */}
-    <span onMouseUp={highlight}>{body}</span>
-
-        {/* maybe on each span, have an onclick or other event handler
-        that will equal to a function above that has window.getSelection()
-        underneath it
-        */}
+            {/* maybe on each span, have an onclick or other event handler
+            that will equal to a function above that has window.getSelection()
+            underneath it
+            */}
     </>
     )
 }
